8058eddf3664331579935980ae6863f9dbac0d9d
NITHINKR06 <nithinpoojari606@gmail.com>
2025-08-18 20:32:29 +0530

    Updated the layout and page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chatbot from "@/components/Chatbot";
 import LandingPage from "@/components/landing/LandingPage";
 import Dashboard from "./user/dashboard/page";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-
+    const router = useRouter();
+  
+    useEffect(() => {
+      const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+      if (!token) {
+        router.push('/login'); // Redirect to login if token is not present
+      }
+    }, [router]);
   return (
     <main className="min-h-screen">
       <Chatbot />
