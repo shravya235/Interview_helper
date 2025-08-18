28c30e9b8f6b26f1f938618ca4a8a84de37fc52a
Shravya R <168519525+shravya235@users.noreply.github.com>
2025-08-19 00:11:02 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import LandingPage from "@/components/landing/LandingPage";
 import Dashboard from "./user/dashboard/page";
-import HelperPage from "@/components/Helper/HelperPage";
+import HelperPage from "./user/helper/page";
 
 export default function Home() {
 
