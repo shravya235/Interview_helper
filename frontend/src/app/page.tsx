74de9d95315ad62cd9f6b0517770176bee68769c
Shravya R <168519525+shravya235@users.noreply.github.com>
2025-08-19 00:06:00 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
-import Chatbot from "@/components/Chatbot";
 import LandingPage from "@/components/landing/LandingPage";
 import Dashboard from "./user/dashboard/page";
+import HelperPage from "@/components/Helper/HelperPage";
 
 export default function Home() {
 
